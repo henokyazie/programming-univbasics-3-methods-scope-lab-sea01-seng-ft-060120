86720980a4f71713575b81ad07e64ba767796165
brave-ping-4201 <h_yazie@yahoo.com>
2020-05-07 02:27:17 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -19,7 +19,7 @@ def all_phrases
   phrase = "It's-a me, Mario!"
   puts phrase
   phrase
-  status = "Thank you Mario! But Our Princess Is In Another Castle!"
+  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
   status
   
